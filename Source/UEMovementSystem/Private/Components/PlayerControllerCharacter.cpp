fb437c3db8d7e905c433828b1a1bb1617ab527a0
Markusssssu <marik334477@mail.ru>
2025-10-11 13:41:31 +0500

    Added PlayerController Source/Private/PlayerControllerCharacter.h

---
+++ Source/UEMovementSystem/Private/Components/PlayerControllerCharacter.cpp
@@ -0,0 +1,51 @@
+#include "Components/PlayerControllerCharacter.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+/*============Constructor(Setup)==============*/
+
+
+APlayerControllerCharacter::APlayerControllerCharacter()
+{
+	
+}
+
+/*===============================================*/
+
+
+/*============PlayerController(Super)==============*/
+
+void APlayerControllerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	DebugPrint("DEBUG[]: Begin Start C++");
+
+}
+
+void APlayerControllerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+/*===============================================*/
+
+/*============PlayerController(DebugSystem)==============*/
+
+void APlayerControllerCharacter::DebugPrint(const FString& Message)
+{
+	if (!GetWorld()) return;
+
+	UKismetSystemLibrary::PrintString(
+		GetWorld(),
+		Message,
+		true,
+		true,
+		DebugGreen,
+		TimeDelta
+	);
+}
+
+/*===============================================*/
+
+
+
---
+++ Source/UEMovementSystem/Public/Components/PlayerControllerCharacter.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerControllerCharacter.generated.h"
+
+
+UCLASS()
+class UEMOVEMENTSYSTEM_API APlayerControllerCharacter : public APlayerController
+{
+	GENERATED_BODY()
+
+	APlayerControllerCharacter();
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	void DebugPrint(const FString& Message);
+
+private:
+	static constexpr FLinearColor DebugGreen{0, 300, 0, 0};
+	static constexpr FLinearColor DebugRed{300, 0, 0, 0};
+
+	static constexpr float TimeDelta = 2.0f;	
+};
